08baf37e7cee4914d1b9bca14f398c478f476caf
Oyewole Oni <onioyewole1910@gmail.com>
2024-02-10 14:05:07 +0100

    Developing Repository Pattern on Basket.API Microservice

---
+++ src/Services/Basket/Basket.API/Repositories/BasketRepository.cs
@@ -0,0 +1,38 @@
+﻿using Basket.API.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.StackExchangeRedis;
+using Newtonsoft.Json;
+
+namespace Basket.API.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IDistributedCache _cache;
+        public BasketRepository(IDistributedCache redisCache)
+        {
+            _cache = redisCache ?? throw new ArgumentNullException(nameof(redisCache));
+        }
+
+        public async Task DeleteBasket(string userName)
+        {
+            await _cache.RemoveAsync(userName);
+        }
+
+        public async Task<ShoppingCart> GetBasket(string userName)
+        {
+            var basket = await _cache.GetStringAsync(userName);
+
+            if (string.IsNullOrEmpty(basket))
+                return null;
+
+            return JsonConvert.DeserializeObject<ShoppingCart>(basket);
+        }
+
+        public async Task<ShoppingCart> UpdateBasket(ShoppingCart basket)
+        {
+            await _cache.SetStringAsync(basket.UserName, JsonConvert.SerializeObject(basket));
+
+            return await GetBasket(basket.UserName);
+        }
+    }
+}
