3a22644bf0eddd4f4b250f2f93fbf9f1ce0ff791
Oyewole Oni <onioyewole1910@gmail.com>
2024-02-22 13:19:03 +0100

    Consuming Discount Grpc Service From Basket Microservice When Adding Cart Item 2

---
+++ src/Services/Basket/Basket.API/GrpcServices/DiscountGrpcService.cs
@@ -0,0 +1,21 @@
+﻿using Discount.Grpc.Protos;
+
+namespace Basket.API.GrpcServices
+{
+    public class DiscountGrpcService
+    {
+        private readonly DiscountProtoService.DiscountProtoServiceClient _grpcDiscountClient;
+
+        public DiscountGrpcService(DiscountProtoService.DiscountProtoServiceClient grpcDiscountClient)
+        {
+            _grpcDiscountClient = grpcDiscountClient ?? throw new ArgumentNullException(nameof(grpcDiscountClient));
+        }
+
+        public async Task<CouponModel> GetDiscount(string productName)
+        {
+            var discountRequest = new GetDiscountRequest { ProductName = productName };
+
+            return await _grpcDiscountClient.GetDiscountAsync(discountRequest);
+        }
+    }
+}
