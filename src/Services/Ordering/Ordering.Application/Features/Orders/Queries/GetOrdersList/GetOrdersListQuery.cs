0263b70bd9674e8e11e9c612913a1a8754746b0b
Oyewole Oni <onioyewole1910@gmail.com>
2024-02-28 14:35:41 +0100

    Developing Ordering.Application Layer- Application Features - GetOrdersListQuery

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersList/GetOrdersListQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrdersList
+{
+    public class GetOrdersListQuery : IRequest<IEnumerable<OrderDto>>
+    {
+        public string Username { get; set; }
+
+        public GetOrdersListQuery(string username)
+        {
+            Username = username ?? throw new ArgumentNullException(nameof(username));
+        }
+    }
+}
--- src/Services/Ordering/Ordering.Application/Mappings/MappingProfile.cs
+++ src/Services/Ordering/Ordering.Application/Mappings/MappingProfile.cs
@@ -1,10 +1,15 @@
 ﻿
 using AutoMapper;
+using Ordering.Application.Features.Orders.Queries.GetOrdersList;
+using Ordering.Domain.Entities;
 
 namespace Ordering.Application.Mappings
 {
     public class MappingProfile : Profile
     {
-        public MappingProfile() { }
+        public MappingProfile()
+        {
+            CreateMap<OrderDto, Order>().ReverseMap();
+        }
     }
 }
