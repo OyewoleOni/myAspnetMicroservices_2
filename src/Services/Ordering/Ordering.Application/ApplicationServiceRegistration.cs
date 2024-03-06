c094e163fe2010b1c70f0fbc51bbbb3537a7caac
Oyewole Oni <onioyewole1910@gmail.com>
2024-03-06 19:14:53 +0100

    Register Application and Infrastructure Layer Dependencies into Ordering.API

--- src/Services/Ordering/Ordering.Application/ApplicationServiceRegistration.cs
+++ src/Services/Ordering/Ordering.Application/ApplicationServiceRegistration.cs
@@ -10,7 +10,7 @@ namespace Ordering.Application
 {
     public static class ApplicationServiceRegistration
     {
-        public static IServiceCollection ApplicationServices(this IServiceCollection services)
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
         {
 
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
