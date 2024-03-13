02ffd091b2acf45440db84854de6cc8e65e3b93e
Oyewole Oni <onioyewole1910@gmail.com>
2024-03-13 14:02:10 +0100

    Developing BuildingBlocks EventBus.Messages Class Library

---
+++ src/BuildingBlocks/EventBus.Message/Events/IntegrationBaseEvent.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace EventBus.Message.Events
+{
+    public class IntegrationBaseEvent
+    {
+        public IntegrationBaseEvent()
+        {
+            Id = Guid.NewGuid();
+            CreatedDate = DateTime.UtcNow;
+        }
+
+        public IntegrationBaseEvent(Guid id,DateTime createdDate)
+        {
+            Id = id;
+            CreatedDate = createdDate;
+        }
+        public Guid Id { get; set; }
+        public DateTime CreatedDate { get; set; } 
+    }
+}
