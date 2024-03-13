c5bb5dd11eb62b9ca5e21bb40cee15397947797e
Oyewole Oni <onioyewole1910@gmail.com>
2024-03-13 15:49:21 +0100

    Produce RabbitMQ Event From Basket Microservice Publisher of BasketCheckoutEvent

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,24 +1,42 @@
 using Basket.API.GrpcServices;
 using Basket.API.Repositories;
 using Discount.Grpc.Protos;
+using MassTransit;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+//Redis Configuration
 builder.Services.AddStackExchangeRedisCache(opt =>
 {
     opt.Configuration = builder.Configuration.GetValue<string>("CacheSettings:ConnectionString");
 });
 
 
+//General Configurations
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+
+
+//Registering DiscountGrpcService
 builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(opt =>
 {
     opt.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"] ?? throw new Exception("Configration Not found"));
 });
-
 builder.Services.AddScoped<DiscountGrpcService>();
+
+
+
+//MassTransit-RabbitMQ Configurationd
+builder.Services.AddMassTransit(config =>
+{
+    config.UsingRabbitMq((ctx, cfg) =>
+    {
+        cfg.Host(builder.Configuration.GetValue<string>("EventBusSettings:HostAddress"));
+    });
+});
+
+builder.Services.AddMassTransitHostedService();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
